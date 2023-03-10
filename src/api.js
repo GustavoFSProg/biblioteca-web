c3d8b609e680d4e882d490f118aac7534888247e
Gustavo <gustavo.prog40@gmail.com>
2023-03-10 16:02:20 -0300

    varginea

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:'https://biblioteca-lfiqebapr-gustavofsprog.vercel.app/',
+  baseURL:'https://biblioteca-api.vercel.app/',
   // baseURL:'http://localhost:5000/',
 })
 
